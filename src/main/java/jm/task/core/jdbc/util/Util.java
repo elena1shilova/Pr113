7feebb8952f08c652888efe1596efad9ec89e85f
elena1989 <elena.shilova89@gmail.com>
2022-10-04 13:03:07 +0300

    первый способ подключения в БД

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static void main(String[] args) {
+        getConnection();
+    }
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tableuser", "root", "Shikarin2022!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        // работа с базой данных
+        System.out.println("EEEEEEs");
+        return conn;
+    }
+
 }
