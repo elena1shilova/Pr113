f50b1120dd5969c4bf172fc80c083365161525eb
elena1989 <elena.shilova89@gmail.com>
2022-10-04 20:17:10 +0300

    сохраняются пустые строки

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,19 +8,21 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
         Util util = new Util();
-        Util.getConnection();
+        //Util.getConnection();
         UserDao userDao = new UserDaoJDBCImpl();
 
-        userDao.createUsersTable();
+        //userDao.createUsersTable();
 
         userDao.saveUser("Name1", "LastName1", (byte) 20);
         userDao.saveUser("Name2", "LastName2", (byte) 25);
         userDao.saveUser("Name3", "LastName3", (byte) 31);
         userDao.saveUser("Name4", "LastName4", (byte) 38);
 
-        userDao.removeUserById(1);
+        /*userDao.removeUserById(1);
         userDao.getAllUsers();
         userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userDao.dropUsersTable();*/
     }
+
+
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,50 @@
 package jm.task.core.jdbc.dao;
-
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
+    Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() { //создать табл пользователей
 
     }
 
-    public void dropUsersTable() {
+    public void dropUsersTable() { //удалить табл пользователей
 
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) { //сохр пользователя
+
+        String mysql = "INSERT INTO users (name, lastName, age) VALUES (name, lastName, age)";
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(mysql);
+            System.out.println("gggg");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) { //удалить юзера по ид
 
     }
 
     public List<User> getAllUsers() {
         return null;
-    }
+    } //список всех адресов
 
-    public void cleanUsersTable() {
+
+    public void cleanUsersTable() { //чистая т ю
 
     }
 }
