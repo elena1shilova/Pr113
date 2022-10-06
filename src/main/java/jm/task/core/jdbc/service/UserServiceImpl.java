f933626b2db2c25e2ad23b50f7f4f52c1ee8ba9b
elena1989 <elena.shilova89@gmail.com>
2022-10-06 11:26:33 +0300

    начало работы с Hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -8,29 +9,30 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDao userDaoJDBCImpl = new UserDaoJDBCImpl();
+    //private final UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
     public void createUsersTable() throws SQLException {
-        userDaoJDBCImpl.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() throws SQLException {
-        userDaoJDBCImpl.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        userDaoJDBCImpl.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) throws SQLException {
-        userDaoJDBCImpl.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() throws SQLException {
 
-        return userDaoJDBCImpl.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() throws SQLException {
-        userDaoJDBCImpl.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
